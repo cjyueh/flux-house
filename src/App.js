8c20c82b9bbdbbeda7a616a397bb0884c30edc33
cjyueh <cjyueh@gmail.com>
2018-01-08 16:27:27 -0800

    add config

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const config = {
+  url: 'http://localhost:8080',
+  flux_url: 'https://flux.io',
+  flux_client_id: '4a9763c3-eab9-4236-a8a7-930f589a12a4'
+}
+
 class App extends Component {
   render() {
     return (
